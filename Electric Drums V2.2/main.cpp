ff7bc27fa231dd2fd685b4b423448f0e89c9f0ca
coloradomacs1 <owen@coloradomacs.com>
2023-07-15 18:35:45 -0500

    df

--- Electric Drums V2.2/main.cpp
+++ Electric Drums V2.2/main.cpp
@@ -5,31 +5,66 @@
 #include <algorithm>
 #include <cmath>
 #include <chrono>
+#include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define BASE 100
 #define SPI_CHAN 0
 
-std::vector<int> thresholds = { 100, 100, 100, 100, 100 };
-std::vector<int> maxThresholds = { 1023, 1023, 1023, 1023, 1023 };
+std::vector<int> thresholds = { 30, 30, 30, 30, 30 };
+std::vector<int> maxThresholds = { 900, 900, 900, 900, 900 };
 std::vector<int> last_values(5, 0);
 std::vector<bool> was_increasing(5, false);
 std::vector<int> peak_values(5, 0);
 std::vector<float> hp_filter_buffer(5, 0);
 std::vector<float> lp_filter_buffer(5, 0);
 std::vector<std::chrono::steady_clock::time_point> last_increase_time(5);
-const double peak_drop_percent = 0.4; // 40%
-const double highpass_coeff = 0.9; // highpass filter coefficient
-const double lowpass_coeff = 0.1; // lowpass filter coefficient
+const double peak_drop_percent = 0.6;
+const double highpass_coeff = 0.99;
+const double lowpass_coeff = 0.05;
 const std::chrono::milliseconds peak_hold_time(2);
-double sensitivity = 1.0; // Adjust this to change sensitivity
+double sensitivity = 1;
 
 double envelope_alpha = 0.1;
 double min_alpha = 0.1;
 double max_alpha = 0.9;
 
+// Serial device parameters
+const char *device = "/dev/ttyAMA0";
+int baud = B9600;
+int serial_port;
+
+void setup() {
+    if (wiringPiSetup() == -1)
+        exit(1);
+    mcp3004Setup(BASE, SPI_CHAN);
+
+    serial_port = open(device, O_RDWR);
+
+    if (serial_port < 0) {
+        std::cerr << "Unable to open serial device: " << device << std::endl;
+        exit(1);
+    }
+
+    struct termios options;
+    tcgetattr(serial_port, &options);
+    cfsetispeed(&options, baud);
+    cfsetospeed(&options, baud);
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    tcsetattr(serial_port, TCSANOW, &options);
+}
+
 void printMidiMessage(int command, int channel, int data1, int data2) {
-    int status = command | channel;
-    std::cout << "Status: " << status << ", Data1: " << data1 << ", Data2: " << data2 << std::endl;
+    char message[3];
+    message[0] = command | channel;
+    message[1] = data1 & 0x7F;
+    message[2] = data2 & 0x7F;
+    write(serial_port, message, 3);
 }
 
 void printNoteOn(int channel, int note, int velocity) {
@@ -40,26 +75,14 @@ void printNoteOff(int channel, int note) {
     printMidiMessage(0x80, channel, note, 0);
 }
 
-void setup() {
-    if (wiringPiSetup() == -1)
-        exit(1);
-    mcp3004Setup(BASE, SPI_CHAN);
-}
-
 void loop() {
     for (int channel = 0; channel < 5; ++channel) {
-        if (channel == 1 || channel == 2 || channel == 4 || channel == 5 || channel == 6) {
-            continue;
-        }
-
         int raw_value = analogRead(BASE + channel);
 
-        // Apply high-pass filter on channel 0
         if (channel == 0) {
             raw_value -= hp_filter_buffer[channel] * highpass_coeff;
             hp_filter_buffer[channel] = raw_value;
         }
-        // Apply low-pass filter on other channels
         else if (channel == 3 || channel == 5) {
             raw_value = raw_value * lowpass_coeff + lp_filter_buffer[channel] * (1.0 - lowpass_coeff);
             lp_filter_buffer[channel] = raw_value;
@@ -85,7 +108,7 @@ void loop() {
                     int adjusted_velocity = std::min(127, std::max(0, (int)(peak_values[channel] * 127 * sensitivity / 1023)));
 
                     printNoteOn(channel, note_number, adjusted_velocity);
-                    delay(25);
+                    usleep(25000);
                     printNoteOff(channel, note_number);
                 }
             }
@@ -100,5 +123,6 @@ int main() {
     while (true) {
         loop();
     }
+    close(serial_port);
     return 0;
 }
